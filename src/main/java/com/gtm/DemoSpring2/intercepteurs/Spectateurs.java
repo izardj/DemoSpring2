8db53260c00423fbc866f223adbfefce6042860e
izardj <izard.jerome@free.fr>
2017-05-18 11:17:18 +0200

    Ajout de la programmation oriente aspect avec Spring

---
+++ src/main/java/com/gtm/DemoSpring2/intercepteurs/Spectateurs.java
@@ -0,0 +1,16 @@
+package com.gtm.DemoSpring2.intercepteurs;
+
+public class Spectateurs {
+
+	public Spectateurs(){
+		
+	}
+	
+	public void prendPlace(){
+		System.out.println("Les spectateurs prennent place");
+	}
+	
+	public void applaudit(){
+		System.out.println("Les spectateurs applaudissent");
+	}
+}
